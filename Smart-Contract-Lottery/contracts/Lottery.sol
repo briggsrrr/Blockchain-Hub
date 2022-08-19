00cb6ae7a0d29435b2a2ca364d35aab659d54f26
briggsrr <robert.briggs.3@icloud.com>
2022-08-19 11:13:05 -0700

    Blockchain lottery restart

--- Smart-Contract-Lottery/contracts/Lottery.sol
+++ Smart-Contract-Lottery/contracts/Lottery.sol
@@ -3,15 +3,26 @@
 pragma solidity >=0.6.6 <0.9.0;
 
 import "../node_modules/@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "../node_modules/@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract Lottery {
     address payable[] public players;
     uint256 public usdEntryFee;
     AggregatorV3Interface internal ethUsdFeed;
 
+    enum LOTTERY_STATE {
+        OPEN,           
+        CLOSED,         
+        CALCULATING_WINNER
+    }
+
+    LOTTERY_STATE public lottery_state;
+
     constructor(address _priceFeedAddress) {
         usdEntryFee = 50 * (10**18);
         ethUsdFeed = AggregatorV3Interface(_priceFeedAddress);
+        lottery_state = LOTTERY_STATE.CLOSED;
+        
     }
 
     function getEntryPrice() public view returns (uint256) {
@@ -32,12 +43,16 @@ contract Lottery {
 
 
     function enter() public payable {
+        require(lottery_state == LOTTERY_STATE.CLOSED, "Lottery state is closed");
         //$50 minimum
-        require(msg.value >= getEntryPrice(), "Not enough Eth!");
+        require(msg.value >= getEntryPrice(), "Need more Eth!");
         players.push(payable(msg.sender));
     }
 
-    function startLottery() public {}
+    function startLottery() public onlyOwner {
+        require(lottery_state == LOTTERY_STATE.CLOSED, "Lottery state is closed");
+    
+    }
     
     function endLottery() public {}
 }
